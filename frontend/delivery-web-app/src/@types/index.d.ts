b371c981f1a7a1869ad763fcc09161ee6aef6ecc
caiquequaresmasilva <caique.quaresma@gmail.com>
2024-03-20 13:17:00 -0300

    feat(Front): Implements AuthFormSubmitButton component

--- frontend/delivery-web-app/src/@types/index.d.ts
+++ frontend/delivery-web-app/src/@types/index.d.ts
@@ -8,5 +8,6 @@ type PostUserResponse = {
 type FieldName = 'email' | 'password'
 type SetState<T> = React.Dispatch<React.SetStateAction<T>>;
 type OnChangeType = React.ChangeEvent<HTMLInputElement>;
+type AuthPaths = '/login' | '/signup'
 // SERVICES TYPES
 type PostUser = (body: PostUser) => Promise<PostUserResponse>;
