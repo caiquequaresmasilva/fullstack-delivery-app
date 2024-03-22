ff534a5571c63f762603e2919ca6556a3b372ed9
caiquequaresmasilva <caique.quaresma@gmail.com>
2024-03-22 16:42:58 -0300

    feat(Back): Implements BcryptHashManager

--- backend/src/application/adapters/HashManager.ts
+++ backend/src/application/adapters/HashManager.ts
@@ -1,4 +1,4 @@
 export interface HashManager {
-  generate(str: string): string
-  compare(str: string, hash: string): boolean
+  generate(str: string): Promise<string>;
+  compare(str: string, hash: string): Promise<boolean>;
 }
