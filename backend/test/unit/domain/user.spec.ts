33a9950154c6212706e190c7038613ce229a62ff
caiquequaresmasilva <caique.quaresma@gmail.com>
2024-03-22 11:41:30 -0300

    test(Back): creates User entity unit test

---
+++ backend/test/unit/domain/user.spec.ts
@@ -0,0 +1,19 @@
+import { User } from '../../../src/domain';
+
+describe('User domain entity', () => {
+  const user = new User({
+    email: 'test@email.com',
+    password: 'jhJkjhkKJ3243',
+    role: 'customer',
+  });
+  it('Should be able to create an User entity', () => {
+    expect(user).toBeTruthy();
+  });
+
+  it('Should be able to return the right properties', () => {
+    expect(user.id).toBe('');
+    expect(user.email).toBe('test@email.com');
+    expect(user.hashedPassword).toBe('jhJkjhkKJ3243');
+    expect(user.role).toBe('customer');
+  });
+});
