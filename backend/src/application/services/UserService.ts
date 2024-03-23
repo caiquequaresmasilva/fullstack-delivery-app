f3444bbab6140143b2d0b655fcbd53cbb5e7cd7e
caiquequaresmasilva <caique.quaresma@gmail.com>
2024-03-23 11:54:04 -0300

    refact(Back): change _makeResponse params

--- backend/src/application/services/UserService.ts
+++ backend/src/application/services/UserService.ts
@@ -10,36 +10,54 @@ export default class UserService {
     private readonly hash: HashManager,
   ) {}
 
-  private _makeResponse(email: string, role: Role, id: string): UserResponse {
+  private _makeResponse({
+    email,
+    name,
+    role,
+    id,
+  }: TokenPayload): UserResponse {
     return {
-      token: this.token.generate({ email, role, id }),
+      name,
+      email,
+      token: this.token.generate({ name, email, role, id }),
       role,
     };
   }
 
-  async create({ email, password, role }: UserProps): Promise<UserResponse> {
+  async create({
+    email,
+    password,
+    role,
+    name,
+  }: UserProps): Promise<UserResponse> {
     const userCheck = await this.repo.findByEmail(email);
     if (userCheck) {
       throw new UserAlreadyExistsError();
     }
     const toCreate = new User({
+      name,
       email,
       role,
-      password: this.hash.generate(password),
+      password: await this.hash.generate(password),
     });
 
     const { id } = await this.repo.create(toCreate);
-    return this._makeResponse(email, role, id);
+    return this._makeResponse({ name, email, role, id });
   }
 
   async login({
     email,
     password,
-  }: Omit<UserProps, 'role'>): Promise<UserResponse> {
+  }: LoginProps): Promise<UserResponse> {
     const user = await this.repo.findByEmail(email);
     if (!user || !this.hash.compare(password, user.hashedPassword)) {
       throw new PasswordEmailError();
     }
-    return this._makeResponse(email, user.role as Role, user.id);
+    return this._makeResponse({
+      name: user.name,
+      email,
+      role: user.role as Role,
+      id: user.id,
+    });
   }
 }
