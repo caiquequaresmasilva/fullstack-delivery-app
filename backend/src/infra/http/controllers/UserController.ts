080a263c36e68f1dbe8a0a4c38d6425309bd7cd5
caiquequaresmasilva <caique.quaresma@gmail.com>
2024-03-23 11:54:44 -0300

    feat(Back): Implements UserController class

---
+++ backend/src/infra/http/controllers/UserController.ts
@@ -0,0 +1,27 @@
+import { NextFunction, Request, Response } from 'express';
+import UserService from '../../../application/services/UserService';
+import { HttpStatus } from '../../enums';
+
+export default class UserController {
+  constructor(private readonly service: UserService) {}
+
+  async create(req: Request, res: Response, next: NextFunction) {
+    const { email, password, role, name } = req.body;
+    try {
+      const response = await this.service.create({name, email, password, role });
+      res.status(HttpStatus.CREATED).json(response);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async login(req: Request, res: Response, next: NextFunction) {
+    const { email, password } = req.body;
+    try {
+      const response = await this.service.login({ email, password });
+      res.status(HttpStatus.OK).json(response);
+    } catch (error) {
+      next(error);
+    }
+  }
+}
