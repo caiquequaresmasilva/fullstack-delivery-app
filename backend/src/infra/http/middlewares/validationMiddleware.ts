f5c7e518bba0b951f1d4617f5ea28cda08d614a8
caiquequaresmasilva <caique.quaresma@gmail.com>
2024-03-23 11:56:01 -0300

    feat(Back): Implements Joi validation middleware

---
+++ backend/src/infra/http/middlewares/validationMiddleware.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, Response } from 'express';
+import { userSchema } from '../../schemas';
+import { HttpStatus } from '../../enums';
+
+export async function validationHandler(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> {
+  const { email, password, role, name } = req.body;
+  const { error } = userSchema.validate({ role, email, password, name });
+  if (error) {
+    res.status(HttpStatus.BAD_REQUEST).json({ error: error.message });
+  } else {
+    next();
+  }
+}
