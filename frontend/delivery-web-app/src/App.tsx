39d56664f258eabf13568dbfa3817a55f409c2ff
caiquequaresmasilva <caique.quaresma@gmail.com>
2024-03-25 21:18:15 -0300

    feat(Front): implements Signup page

--- frontend/delivery-web-app/src/App.tsx
+++ frontend/delivery-web-app/src/App.tsx
@@ -3,6 +3,7 @@ import { Login } from "./pages/Login"
 import { CustomerProducts } from "./pages/customer-pages"
 import { SellerOrders } from "./pages/seller-pages"
 import { AdminManage } from "./pages/admin-pages"
+import { Signup } from "./pages/Signup"
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <Routes>
       <Route path='/' element={<Navigate replace to='/login' />} />
       <Route path='/login' element={<Login />} />
+      <Route path='/signup' element={<Signup />} />
       <Route path='/customer/products' element={<CustomerProducts />} />
       <Route path='/seller/orders' element={<SellerOrders />} />
       <Route path='/admin/manage' element={<AdminManage />} />
