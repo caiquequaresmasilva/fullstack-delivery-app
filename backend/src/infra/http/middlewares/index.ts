e70c62373517f02598df0f4cb80308309e30bdd1
caiquequaresmasilva <caique.quaresma@gmail.com>
2024-03-25 23:06:31 -0300

    feat(Back): implements RoleGuard class

--- backend/src/infra/http/middlewares/index.ts
+++ backend/src/infra/http/middlewares/index.ts
@@ -1,3 +1,4 @@
 export { default as CreateUserGuard } from './CreateUserGuard';
+export { default as RoleGuard } from './RoleGuard';
 export { default as errorMiddleware } from './errorMiddleware';
 export * from './validationMiddleware';
