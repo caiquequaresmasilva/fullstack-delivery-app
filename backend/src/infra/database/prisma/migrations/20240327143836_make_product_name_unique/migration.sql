7718d0febd8186f47c318182036edf52d0091638
caiquequaresmasilva <caique.quaresma@gmail.com>
2024-03-27 22:04:11 -0300

    chore(Back): reset migrations

--- backend/src/infra/database/prisma/migrations/20240327143836_make_product_name_unique/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[name]` on the table `DeliveryProduct` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "DeliveryProduct_name_key" ON "DeliveryProduct"("name");
