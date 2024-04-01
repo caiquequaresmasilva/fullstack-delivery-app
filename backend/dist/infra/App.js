a91d24aa075f3e45896811fe3ae10f685f97cac5
caiquequaresmasilva <caique.quaresma@gmail.com>
2024-04-01 18:34:30 -0300

    chore: update build

--- backend/dist/infra/App.js
+++ backend/dist/infra/App.js
@@ -3,6 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.app = void 0;
 const express_1 = __importDefault(require("express"));
 const morgan_1 = __importDefault(require("morgan"));
 const cors_1 = __importDefault(require("cors"));
@@ -42,3 +43,4 @@ class App {
     }
 }
 exports.default = App;
+exports.app = new App().app;
