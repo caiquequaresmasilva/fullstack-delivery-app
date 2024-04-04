f42939c27590ffd5ea489d287e9bc84e4ea4c494
caiquequaresmasilva <caique.quaresma@gmail.com>
2024-04-03 23:25:19 -0300

    chore:update build

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -5,5 +5,5 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const App_1 = __importDefault(require("./infra/App"));
 require("dotenv/config");
-const PORT = process.env.API_PORT || 3001;
+const PORT = process.env.API_PORT || 3333;
 new App_1.default().start(PORT);
