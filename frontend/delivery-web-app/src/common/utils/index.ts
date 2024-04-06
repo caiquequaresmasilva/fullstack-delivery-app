01fbd24b1aa2b9997e8b511a31c072c817862c34
caiquequaresmasilva <caique.quaresma@gmail.com>
2024-04-05 23:50:10 -0300

    feat(Front): implements extract from token utils

--- frontend/delivery-web-app/src/common/utils/index.ts
+++ frontend/delivery-web-app/src/common/utils/index.ts
@@ -1,4 +1,5 @@
 export { default as capitalize } from './capitalize';
 export * from './debounceOnChange';
+export { default as extractFromToken } from './extractFromToken';
 export * from './roleToPathMap';
 export { default as validateField } from './validateField';
