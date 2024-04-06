54684e98f1e236798990364f4279bc115de97a04
caiquequaresmasilva <caique.quaresma@gmail.com>
2024-04-06 00:51:13 -0300

    feat(Front): configures routes

---
+++ frontend/delivery-web-app/src/pages/customer-pages/CustomerOrderDetails.tsx
@@ -0,0 +1,5 @@
+export default function CustomerOrderDetails() {
+  return (
+    <div>CustomerOrderDetails</div>
+  )
+}
---
+++ frontend/delivery-web-app/src/pages/seller-pages/components/SellerHeader.tsx
@@ -0,0 +1,9 @@
+import { Header } from "../../../common/components/header";
+
+export default function SellerHeader() {
+  return (
+    <Header.container>
+      <Header.navigation label="ORDERS"/>
+    </Header.container>
+  )
+}
